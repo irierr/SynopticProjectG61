4c523426b10665cf21fce16f01a3c6bd999685fe
irie612 <57913132+irie612@users.noreply.github.com>
2021-06-07 12:36:17 +0100

    added basic ui

--- App/src/main/java/org/synoptic/Main.java
+++ App/src/main/java/org/synoptic/Main.java
@@ -1,8 +1,26 @@
 package org.synoptic;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static final int DEFAULT_WIDTH = 400;
+    public static final int DEFAULT_HEIGHT = 800;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent homeRoot = FXMLLoader.load(getClass().getResource("/view.fxml"));
+        Scene homeScene = new Scene(homeRoot, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        stage.setScene(homeScene);
+        stage.setFullScreen(false);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
---
+++ App/src/main/java/org/synoptic/controller.java
@@ -0,0 +1,14 @@
+package org.synoptic;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
