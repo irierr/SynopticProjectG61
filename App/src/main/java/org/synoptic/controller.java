c2cbbd27797ac41a750e4120af976f74047ac905
IrieR <175622981+irierr@users.noreply.github.com>
2021-06-15 14:10:03 +0100

    Added working zoom to map

--- App/src/main/java/org/synoptic/controller.java
+++ App/src/main/java/org/synoptic/controller.java
@@ -1,20 +1,17 @@
 package org.synoptic;
 
-import javafx.event.Event;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 import javafx.stage.Stage;
-import javafx.scene.input.MouseEvent;
-import javafx.event.ActionEvent;
-import javafx.scene.control.*;
-import javafx.stage.Window;
-import javafx.scene.control.TabPane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -22,13 +19,40 @@ import java.util.ResourceBundle;
 
 public class controller implements Initializable {
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
     public static final int DEFAULT_WIDTH = 400;
     public static final int DEFAULT_HEIGHT = 800;
 
+    public ImageView mapImage = new ImageView();
+    public ScrollPane scroller = new ScrollPane();
+    final DoubleProperty zoomProperty = new SimpleDoubleProperty(400);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initMap();
+    }
+
+    private void initMap(){
+        scroller.setHvalue(0.5);
+        scroller.setVvalue(1);
+        mapImage.preserveRatioProperty().set(true);
+
+        zoomProperty.addListener(arg0 -> {
+            double oldV = scroller.getVvalue() ;
+            double oldH = scroller.getHvalue();
+            mapImage.setFitWidth(zoomProperty.get() * 4);
+            mapImage.setFitHeight(zoomProperty.get() * 3);
+            scroller.setVvalue(oldV);
+            scroller.setHvalue(oldH);
+        });
+
+        scroller.addEventFilter(ScrollEvent.SCROLL, event -> {
+            if ((event.getDeltaY() > 0) && (mapImage.getFitHeight() < 2130)) {
+                zoomProperty.set(zoomProperty.get() * 1.1);
+            } else if ((event.getDeltaY() < 0) && (mapImage.getFitWidth() > 1455)) {
+                zoomProperty.set(zoomProperty.get() / 1.1);
+            }
+        });
+    }
 
     public Scene loadScene(String path) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource(path));
