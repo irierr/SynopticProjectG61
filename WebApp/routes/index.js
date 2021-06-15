550edb848ee675b94f4661c9d73edf9889828693
darceygardiner99 <57676112+darceygardiner99@users.noreply.github.com>
2021-06-15 16:57:06 +0100

    Add layout finalised

--- WebApp/routes/index.js
+++ WebApp/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('add', { title: 'Add' });
+});
+
+router.get('/update', function(req, res, next) {
+  res.render('update', { title: 'Update' });
 });
 
 module.exports = router;
