b040155fe98cacb025e2a402251cf88e9fd0aea4
Brandon Gous <fierchecer@gmail.com>
2021-06-16 16:31:29 +0100

    More database additions missing from the last commit

--- WebApp/database.js
+++ WebApp/database.js
@@ -1,17 +1,19 @@
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
+const sql = require('mysql');
+
+const connection = sql.createConnection({
     host: '81.104.43.46',
     port: '3306',
     database: 'ecoSwellDatabase',
     user:'appUser',
-    password: '9iPTs#ko',
-});
+    password: '9iPTs#ko'
+})
 
-async function asyncFunction() {
-    try {
-        let conn = await pool.getConnection();
-        console.log('connected!');
-    } catch (err) {
-        throw err;
+connection.connect((err) => {
+    if (err) {
+        console.log('SQL Connection error');
     }
-}
+
+    console.log('SQL Connected')
+})
+
+module.exports = connection;
